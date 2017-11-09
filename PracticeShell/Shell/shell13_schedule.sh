2f8039e945514076e83999c22c5ccaf7a843e6c8
Renleilei_Johnny <renleilei1992@foxmail.com>
2017-11-09 10:44:38 +0800

    modified the shell13_schedule.sh! :)

--- PracticeShell/Shell/shell13_schedule.sh
+++ PracticeShell/Shell/shell13_schedule.sh
@@ -6,13 +6,41 @@ echo "0%           25%            50%           75%         100%"
 
 
 sleep 1
-echo "||\c"
+echo "|\c"
 sleep 1
+echo "|\c"
+sleep 1
+echo "|\c"
+sleep 1
+echo "|\c"
+sleep 1
+echo "|\c"
+sleep 1
+echo "|\c"
+sleep 1
+echo "|\c"
+sleep 1
+echo "|\c"
+sleep 1
+echo "|\c"
+sleep 1
+echo "|\c"
+sleep 1
+echo "|\c"
+sleep 1
+echo "|\c"
+sleep 1
+echo "|\c"
+sleep 1
+echo "|\c"
+sleep 1
+echo "|\c"
+echo "||\c"
 echo "|||||\c"
 sleep 2
 echo "|||||||||||||||||\c"
 sleep 5
-echo "||||||||||||||||||||||||||\c"
+echo "|||||||||||\c"
 sleep 2
 echo "||||||||"
 

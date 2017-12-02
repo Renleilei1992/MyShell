309fdf8a6869946db53f72266d1a78230b7f452b
Renleilei_Johnny <renleilei1992@foxmail.com>
2017-12-02 22:49:07 +0800

    commit a shell! :)

---
+++ PracticeShell/Shell/shell17_createUser.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#================================================================
+#   Copyright (C) 2017 Renleilei. All rights reserved.
+#   
+#   文件名称：shell17_createUser.sh
+#   创 建 者：Renleilei (renleilei@foxmail.com)
+#   创建日期：2017年12月01日
+#   描    述：
+#   版    本: Version 1.00
+#================================================================
+
+

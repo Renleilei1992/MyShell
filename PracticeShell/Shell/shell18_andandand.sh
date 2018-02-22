ab19b9cea6afa443806e553a26c31fc834e9c308
renleilei_mac <renleilei@hujiang.com>
2018-02-22 18:29:39 +0800

    shell18.sh commit

---
+++ PracticeShell/Shell/shell18_andandand.sh
@@ -0,0 +1,16 @@
+#================================================================
+#   Copyright (C) 2018 Renleilei. All rights reserved.
+#   
+#   文件名称：shell18_andandand.sh
+#   创 建 者：Renleilei (renleilei@foxmail.com)
+#   创建日期：2018年02月22日
+#   描    述：
+#   版    本: Version 1.00
+#================================================================
+
+echo "hello my zsh!"
+
+while true
+do
+	echo "hello 001" && echo "hello 002" && echo "hello 003" && exit
+done

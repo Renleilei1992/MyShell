a7c253ec48c7774dad244868ee57e34f91314967
Renleilei_Johnny <renleilei1992@foxmail.com>
2018-05-03 15:13:42 +0800

    Thu May  3 15:13:42 CST 2018 commit

--- PracticeShell/Shell/shell20_crontab.sh
+++ PracticeShell/Shell/shell20_crontab.sh
@@ -17,14 +17,20 @@ echo $date
 echo $filename
 echo $0
 
-echo "#"$date >>./$0
+cd /home/Renleilei/Repository/MyShell/PracticeShell/Shell
 
-ret01=`git add ./$0`
-ret02=`git commit -m "$date commit" ./$0`
-ret03=`git pull origin master`
-ret04=`git push origin master`
+
+echo "#"$date >>/home/Renleilei/Repository/MyShell/PracticeShell/Shell/$filename
+
+
+
+ret01=`git add /home/Renleilei/Repository/MyShell/PracticeShell/Shell/$filename >> /home/Renleilei/Repository/MyShell/PracticeShell/Log/commit.log`
+ret02=`git commit -m "$date commit" /home/Renleilei/Repository/MyShell/PracticeShell/Shell/$filename >> /home/Renleilei/Repository/MyShell/PracticeShell/Log/commit.log`
+ret03=`git pull origin master >> /home/Renleilei/Repository/MyShell/PracticeShell/Log/commit.log`
+ret04=`git push origin master >> /home/Renleilei/Repository/MyShell/PracticeShell/Log/commit.log`
 
 #shell18_crontab.sh
 #Thu May 3 14:01:53 CST 2018
 #Thu May 3 14:03:55 CST 2018
 #Thu May 3 14:05:39 CST 2018
+#Thu May 3 15:13:42 CST 2018

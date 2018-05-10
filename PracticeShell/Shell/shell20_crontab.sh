b66fc02018245a0a81e83f5c1a54d1e5716b6213
Renleilei_Johnny <renleilei1992@foxmail.com>
2018-05-10 20:30:01 +0800

    Thu May 10 20:30:01 CST 2018 commit

--- PracticeShell/Shell/shell20_crontab.sh
+++ PracticeShell/Shell/shell20_crontab.sh
@@ -69,3 +69,4 @@ ret05=`git push origin master >> /home/Renleilei/Repository/MyShell/PracticeShel
 #Thu May 10 10:30:01 CST 2018
 #Thu May 10 12:30:01 CST 2018
 #Thu May 10 14:30:01 CST 2018
+#Thu May 10 20:30:01 CST 2018

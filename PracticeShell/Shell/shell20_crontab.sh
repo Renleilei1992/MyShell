ce21bbf403d2c0977b79245165df7929edf5be35
Johnny_Aliyun_New <renleilei1992@foxmail.com>
2019-02-01 22:30:01 +0800

    Fri Feb  1 22:30:01 CST 2019 commit

--- PracticeShell/Shell/shell20_crontab.sh
+++ PracticeShell/Shell/shell20_crontab.sh
@@ -784,3 +784,4 @@ ret05=`git push origin master >> $LOCAL_LOG_PATH/commit.log`
 #Thu Jan 31 14:30:01 CST 2019
 #Thu Jan 31 22:30:01 CST 2019
 #Fri Feb 1 14:30:01 CST 2019
+#Fri Feb 1 22:30:01 CST 2019
